5402c82a24d804ff7ec9b12784318c1136c04998
Ali Hassan <alihassan057689234@gmail.com>
2025-02-12 13:17:24 +0500

    fix backend and connected to the frontend

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const aiController = require('../controllers/ai.controller');
 
 const router = express.Router();
-const aiController = require('../controllers/ai.controller');
+
 
 router.post('/get-review', aiController.getReview);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import 'prismjs/themes/prism-tomorrow.css'
+import Editor from 'react-simple-code-editor' 
+import prism from 'prismjs'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [code,setCode] = useState(`function sum(){ return 1 + 1;} `);
+  const [review,setReview] = useState('')
+
+  useEffect(() => {
+    prism.highlightAll()
+  },[])
+
+  const reviewCode = async () => {
+      const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+      // console.log(response.data)
+      setReview(response.data)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, 'javascript')}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: '1px solid #e1e1e1',
+                borderRadius: '5px',
+                height: '100%',
+                width: '100%'
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">Review</div>
+        </div>
+        <div className="right">
+          {review}
+        </div>
+      </main>
     </>
   )
 }
